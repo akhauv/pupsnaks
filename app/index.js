6a00def03b6341f8e80cbcc90a69bc7c298bc818
Amber Khauv <akkhauv@gmail.com>
2024-06-13 01:39:43 +0200

    testing commit under new email

--- app/index.js
+++ app/index.js
@@ -2,15 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Image } from 'react-native';
 import * as Font from "expo-font";
 
-import AppText from '../components/AppText'
-import AppScreen from '../components/Screen';
 import fonts from '../config/fonts';
-import HomeScreen from './HomeScreen';
-import AppColorPicker from '../components/AppColorPicker';
-import Pups from '../config/pups';
 import PupScreen from './PupScreen';
-import { AppRegistry } from 'react-native-web';
-import Icon from '../components/Icon';
 
 function index() {
     const [isReady, setIsReady] = useState(false);
