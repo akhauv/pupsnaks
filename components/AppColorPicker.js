d748f3270095093b66df561d37017d841e5fde95
Amber Khauv <akkhauv@gmail.com>
2024-06-14 17:59:07 +0200

    document AppColorPicker

--- components/AppColorPicker.js
+++ components/AppColorPicker.js
@@ -3,15 +3,18 @@ import { StyleSheet, View } from 'react-native';
 import ColorPicker, { HueCircular, Panel1 } from 'reanimated-color-picker'
 
 import colors from '../config/colors';
-import AppText from './AppText';
+
+/*
+ * This app component is a color picker featuring an outer circular hue picker and inner
+ * saturation / darkness picker. Via callback, it can be used to update an outer color state 
+ * variable. 
+ */
 
 function AppColorPicker({ 
     handleChange,
     initialColor = colors.tertiary,
     size = 150
 }) {
-    const [color, setColor] = useState(colors.tertiary);
-
     return (
         <View style={styles.container}>
             <ColorPicker 
@@ -19,15 +22,16 @@ function AppColorPicker({
                 alignItems='center'
                 justifyContent='center'
                 value={initialColor}
-                onChange={color => setColor(color.hex)}
                 onComplete={color => handleChange(color.hex)}
             >
+                {/* outer circular hue selection */}
                 <HueCircular 
                     thumbShape={"hollow"}
                     thumbSize={20}
                     sliderThickness={10}
                     style={[styles.outerPanel, {height: size, width: size}]}
                 >
+                    {/* inner saturation and darkness picker */}
                     <Panel1 style={[styles.innerPanel, {borderRadius: size / 2, margin: 5}]} />
                 </HueCircular>
             </ColorPicker>
